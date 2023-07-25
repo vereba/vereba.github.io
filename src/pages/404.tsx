36f21f846dc9f81432cd7afeded20de195871cc8
Verena Barth <verena.barth@viadee.de>
2023-07-25 13:04:18 +0200

    add -b flag to deploy task

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -10,12 +10,16 @@ import backgroundImage from "../assets/images/favela_100x70.jpg";
 
 const NotFoundPage = () => (
   <Layout pageInfo={{ pageName: "Not found" }}>
-      <div className="page-image-container" id="home-image"
+    <div className="page-image-container" id="home-image"
       style={{
         backgroundImage: `url(${backgroundImage})`,
       }}
     >
-      <p>hi</p>>
+      <div id="not-found-page">
+        <h1>404</h1>
+        <h2>Page not found!</h2>
+      </div>
+
     </div>
   </Layout>
 )
