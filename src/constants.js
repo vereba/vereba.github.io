9928a2dc6268e1cbd7836d1bb0ebb1d5a361f608
vereba <verena.barth95@web.de>
2026-09-22 16:48:25 +0200

    fix: exhibition location

--- src/constants.js
+++ src/constants.js
@@ -51,12 +51,12 @@ module.exports = {
       entries: [
         {
           name: "Großstadt Dschungel (solo)",
-          place: "Bunker K101, Köln (Germany)",
+          place: "Galerie Eyegenart, Köln (Germany)",
           date: "01.05. – 12.05.2026",
         },
         {
           name: "Kunstroute Ehrenfeld",
-          place: "Galerie Eyegenart, Köln (Germany)",
+          place: "Bunker K101, Köln (Germany)",
           date: "27.09. – 22.10.2026",
         },
       ],
